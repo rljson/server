10d8058b718b976932d6393cebdab3ec1c850ee5
Jan Balzer <154504062+Jan-Balzer@users.noreply.github.com>
2026-09-17 16:07:37 +0200

    Add the rljson DNA (#76)
    * #gg: generated DNA
    
    * #gg: generated DNA
    
    * Add the rljson DNA
    
    * #gg: Add .gg/gg.json check results
    
    * Add the rljson DNA
    
    * #gg: Add .gg/gg.json check results
    
    * Update to import.meta.dirname
    
    * #gg: Add .gg/gg.json check results
    
    * update pipeline
    
    * #gg: Add .gg/gg.json check results
    
    * update pipeline again
    
    * #gg: Add .gg/gg.json check results

--- scripts/delete-feature-branch.js
+++ scripts/delete-feature-branch.js
@@ -1,6 +1,6 @@
-/*
+/**
  * @license
- * Copyright (c) 2025 Rljson
+ * Copyright (c) rljson
  *
  * Use of this source code is governed by terms that can be
  * found in the LICENSE file in the root of this package.
